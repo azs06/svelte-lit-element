e8fe97e728f654fe4a4392f4cfde95b032314739
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2022-02-10 01:49:49 +0600

    added lit component

---
+++ src/Component.svelte
@@ -0,0 +1,13 @@
+<script>
+    import '../components/my-elements.js';
+    function increment(){
+        let counter = document.querySelector('my-element');
+        counter._increment();
+    }
+</script>
+
+<div>
+    <h1>Hello, World</h1>
+    <my-element></my-element>
+    <button on:click={increment}>+</button>
+</div>
